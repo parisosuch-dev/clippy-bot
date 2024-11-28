89c74f1517e3eadeba9d31f6011a5921b7129f5e
paris osuch <paris.osuch@gmail.com>
2024-11-27 18:51:59 -0800

    create updateChannelName() function

--- src/lib/supabase/channel.ts
+++ src/lib/supabase/channel.ts
@@ -1,13 +1,23 @@
 import supabaseClient from "./supabase";
-import { Channel } from "./model";
 import { TextChannel } from "discord.js";
 
 export const createChannel = async (channel: TextChannel) => {
-  const { data, error } = await supabaseClient.from("Server").insert({
+  const { data, error } = await supabaseClient.from("Channel").insert({
     id: channel.id,
     server_id: channel.guild.id,
     name: channel.name,
   });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const updateChannelName = async (id: string, name: string) => {
+  const { data, error } = await supabaseClient
+    .from("Channel")
+    .update({ name: name })
+    .eq("id", id);
 
   if (error) {
     throw new Error(error.message);
